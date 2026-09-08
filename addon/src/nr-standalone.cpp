cbe2475d9b3f5557f73afbe3abab41b1c10e9021
David Russ <luckyme1234letsplaysomemore@gmail.com>
2026-09-08 18:05:45 -0400

    Release 2.2.0 experimental NVIDIA Optical Flow

--- addon/src/nr-standalone.cpp
+++ addon/src/nr-standalone.cpp
@@ -36,7 +36,7 @@
 #include "aio-menu-schema.hpp"
 #include "nvof-motion-provider.hpp"
 
-#define ADDON_VERSION "2.2.0-nvof-prototype"
+#define ADDON_VERSION "2.2.0-experimental.1"
 
 extern "C" __declspec(dllexport) const char *NAME = "Standalone DLSS-NR + SR " ADDON_VERSION;
 extern "C" __declspec(dllexport) const char *DESCRIPTION =
--- release/package-release.ps1
+++ release/package-release.ps1
@@ -1,6 +1,6 @@
 param(
     [Parameter(Mandatory = $true)]
-    [ValidatePattern('^v\d+\.\d+\.\d+$')]
+    [ValidatePattern('^v\d+\.\d+\.\d+(?:-[0-9A-Za-z.-]+)?$')]
     [string] $Version
 )
 
